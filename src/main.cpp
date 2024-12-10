ec9415a242ee2974ccd396dfe55e18e66fe8728d
MasterofBed <Jenslyn07@gmail.com>
2024-12-10 13:24:21 +0100

    chenged
    cnhedne

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,36 @@ using namespace std;
 
 string coordinateGrid[11][11];
 char coordinateGrid[11][11];
+int mapSize = 10;
 
 void createGrid() {
     for(int x = 0; x <= 10; ++x) {
         for(int y = 0; y <= 10; ++y) {
+void createGrid(int mx = 404, int my = 404) {
 
             coordinateGrid[x][y] = " x ";
             coordinateGrid[x][y] = "x";
             cout << coordinateGrid[x][y];
+    int lx = 5;
+    int ly = 5;
+
+    if(mx && my == 404) {
+        goto firstSkip;
+    }
+    if(mx != 404) {
+        lx + mx;
+    }
+    if(my != 404) {
+        ly + my;
+    }
+
+    firstSkip:
+    for(int x = 0; x <= mapSize; ++x) {
+        for(int y = 0; y <= mapSize; ++y) {
+
+            coordinateGrid[x][y] = 'x';
+            coordinateGrid[lx][ly] = 'O';
+            cout << ' ' << coordinateGrid[x][y] << ' ';
             if(y == 10) {
                 cout << "\n";
             }
@@ -18,6 +40,14 @@ void createGrid() {
     }
 }
 
+
+
 int main() {
     createGrid();
+    while(true) {
+        createGrid(1, 404);
+        cout << "\n" << "Type direction to move" << "\n" << ">";
+        string penis;
+        cin >> penis;
+    }
 }
