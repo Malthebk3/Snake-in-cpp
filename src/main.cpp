17f34e331f85ff7b0013d594ac4d157cd3821f61
Malthebk3 <makr55@elev.techcollege.dk>
2024-12-12 08:48:06 +0100

    Added Apple

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,88 @@
 #include <iostream>
 #include <windows.h>
-#include <cstdlib>
+#include <deque>
 using namespace std;
 
 char coordinateGrid[11][11];
-int mapSize = 10;
-int lx = 5;
-int ly = 5;
-int currentPosX = 0;
-int currentPosY = 0;
-int ax = 0;
-int ay = 0;
+int mapSize = 10, lx = 5, ly = 5, snakeLenght = 5;
+deque<int> prevPosX = {5, 4, 3, 2, 1}, prevPosY = {5, 5, 5, 5, 5};
 int randAppleSpawnX = rand() % 10;
 int randAppleSpawnY = rand() % 10;
 
-void createGrid(int mx = 0, int my = 0) {
-    ax = randAppleSpawnX;
-    ay = randAppleSpawnY;
-    currentPosX = currentPosX + mx;
-    currentPosY = currentPosY + my;
-    for(int x = 0; x <= mapSize; ++x) {
-        for(int y = 0; y <= mapSize; ++y) {
+void createGrid() {
+    for (int x = 0; x <= mapSize; ++x) {
+        for (int y = 0; y <= mapSize; ++y) {
             coordinateGrid[x][y] = '.';
-            coordinateGrid[ly + currentPosY][lx + currentPosX] = 'O';
-            if((coordinateGrid[ay][ax] = '.')||(coordinateGrid[ay][ax] = 'A')){
-                coordinateGrid[ay][ax] = 'A';
-            }
-            else {
-                randAppleSpawnX = rand() % 10;
-                randAppleSpawnY = rand() % 10;
-                ax = randAppleSpawnX;
-                ay = randAppleSpawnY;
-            }
+        }
+    }
+    for (int i = 0; i < snakeLenght; ++i) {
+        coordinateGrid[prevPosY[i]][prevPosX[i]] = 'O';
+    }
+    for (int x = 0; x <= mapSize; ++x) {
+        for (int y = 0; y <= mapSize; ++y) {
             cout << ' ' << coordinateGrid[x][y] << ' ';
-            if(y == 10) {
-                cout << "\n";
-            }
+            if (y == mapSize) cout << "\n";
         }
     }
 }
 
+void appleSpawn(){
+    int ay = randAppleSpawnY;
+    int ax = randAppleSpawnX;
+    if((coordinateGrid[randAppleSpawnY][randAppleSpawnX] = '.')||(coordinateGrid[ay][ax] = 'A')){
+                coordinateGrid[ay][ax] = 'A';
+    }
+    else { 
+            randAppleSpawnX = rand() % 10;
+            randAppleSpawnY = rand() % 10;
+            ax = randAppleSpawnX;
+            ay = randAppleSpawnY;
+    } 
+}
 
+void moveSnake(int h, int v) {
+    int currentPosX = prevPosX.front() + h;
+    int currentPosY = prevPosY.front() + v;
+    if((coordinateGrid[currentPosY][currentPosX] = 'A')){
+        snakeLenght = snakeLenght + 1;
+    }
+    if (currentPosX < 0 || currentPosX > mapSize || currentPosY < 0 || currentPosY > mapSize) {
+        cout << "Game Over. Snake hit the wall.\n";
+        Sleep(10000);
+        exit(0);
+    }
+    for (int i = 1; i < snakeLenght; ++i) if (prevPosX[i] == prevPosX.front() && prevPosY[i] == prevPosY.front()) {
+        cout << "Game Over. You ran into yourself.\n";
+        Sleep(10000);
+        exit(0);
+    }
 
-int h = 0;
-int v = 0;
+    prevPosX.push_front(currentPosX);
+    prevPosY.push_front(currentPosY);
+
+    if (prevPosX.size() > snakeLenght) prevPosX.pop_back();
+    if (prevPosY.size() > snakeLenght) prevPosY.pop_back();
+}
+
 int main() {
+    int h = 1, v = 0;
     createGrid();
-    while(true) {
+    while (true) {
         cout << " Press WASD to move:" << endl;
-            for (int keyCode = 0; keyCode < 256; ++keyCode) {
-                if (GetAsyncKeyState(keyCode) & 0x8000) {
-                    char keyChar = static_cast<char>(keyCode);
-                    if(keyChar == 'W'){h = 0; v = -1;}
-                    if(keyChar == 'S'){h = 0; v = 1;}
-                    if(keyChar == 'A'){v = 0; h = -1;}
-                    if(keyChar == 'D'){v = 0; h = 1;}
-                }
+        for (int keyCode = 0; keyCode < 256; ++keyCode) {
+            if (GetAsyncKeyState(keyCode) & 0x8000) {
+                char keyChar = static_cast<char>(keyCode);
+                if (keyChar == 'W' && v != 1) { h = 0; v = -1; }
+                if (keyChar == 'S' && v != -1) { h = 0; v = 1; }
+                if (keyChar == 'A' && h != 1) { v = 0; h = -1; }
+                if (keyChar == 'D' && h != -1) { v = 0; h = 1; }
             }
-
-
-        Sleep(500);
-
+        }
+        Sleep(100);
         system("cls");
-        createGrid(h, v);
+        moveSnake(h, v);
+        appleSpawn();
+        createGrid();
     }
 }
+
