237177d3978fb7d31295b7efadb006bc9e112f60
Malthebk <makr55@elev.techcollege.dk>
2024-12-18 10:28:45 +0100

    Fixed flickering issue caused by system("cls")

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,13 @@
 #include <cstdlib>
 using namespace std;
 
-char coordinateGrid[11][11]; //Størrelse på mappet
-int mapSize = 10, lx = 5, ly = 5, snakeLenght = 5, randAppleSpawnX = rand() % 10, randAppleSpawnY = rand() % 10, ay = rand() % 10, ax = rand() % 10; //global variables
-deque<int> prevPosX = {5, 4, 3, 2, 1}, prevPosY = {5, 5, 5, 5, 5}; //Tidligere positioner af hovedet/hale position
+char coordinateGrid[11][11];
+int mapSize = 10, lx = 5, ly = 5, snakeLenght = 5, randAppleSpawnX = rand() % 10, randAppleSpawnY = rand() % 10, ay = rand() % 10, ax = rand() % 10;
+deque<int> prevPosX = {5, 4, 3, 2, 1}, prevPosY = {5, 5, 5, 5, 5};
 
-void appleSpawn() { //
+HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void appleSpawn() {
     do {
         randAppleSpawnX = rand() % 10;
         randAppleSpawnY = rand() % 10;
@@ -18,15 +20,18 @@ void appleSpawn() { //
 }
 
 void createGrid() {
-    for (int x = 0; x <= mapSize; ++x) { //Lav et grid ud af "."
+    COORD cursorPos = {0, 0};
+    SetConsoleCursorPosition(consoleHandle, cursorPos); // Move cursor to the top-left corner
+
+    for (int x = 0; x <= mapSize; ++x) {
         for (int y = 0; y <= mapSize; ++y) {
             coordinateGrid[x][y] = '.';
         }
     }
     for (int i = 0; i < snakeLenght; ++i) {
-        coordinateGrid[prevPosY[i]][prevPosX[i]] = 'O'; //indsætter slangens position fra deque
+        coordinateGrid[prevPosY[i]][prevPosX[i]] = 'O';
     }
-    coordinateGrid[ay][ax] = 'A'; //indsætter apples 
+    coordinateGrid[ay][ax] = 'A';
     for (int x = 0; x <= mapSize; ++x) {
         for (int y = 0; y <= mapSize; ++y) {
             cout << ' ' << coordinateGrid[x][y] << ' ';
@@ -35,11 +40,11 @@ void createGrid() {
     }
 }
 
-void moveSnake(int h, int v) { 
-    int currentPosX = prevPosX.front() + h; 
+void moveSnake(int h, int v) {
+    int currentPosX = prevPosX.front() + h;
     int currentPosY = prevPosY.front() + v;
-    if(coordinateGrid[currentPosY][currentPosX] == 'A'){
-        snakeLenght = snakeLenght + 1;
+    if (coordinateGrid[currentPosY][currentPosX] == 'A') {
+        snakeLenght++;
         appleSpawn();
     }
     if (currentPosX < 0 || currentPosX > mapSize || currentPosY < 0 || currentPosY > mapSize) {
@@ -47,36 +52,32 @@ void moveSnake(int h, int v) {
         Sleep(10000);
         exit(0);
     }
-    for (int i = 1; i < snakeLenght; ++i) if (prevPosX[i] == prevPosX.front() && prevPosY[i] == prevPosY.front()) {
-        cout << "Game Over. You ran into yourself.\n";
-        Sleep(10000);
-        exit(0);
-    }
+    for (int i = 1; i < snakeLenght; ++i)
+        if (prevPosX[i] == currentPosX && prevPosY[i] == currentPosY) {
+            cout << "Game Over. You ran into yourself.\n";
+            Sleep(10000);
+            exit(0);
+        }
 
-    prevPosX.push_front(currentPosX); //hovedets positioner gemmes her
+    prevPosX.push_front(currentPosX);
     prevPosY.push_front(currentPosY);
 
-    if (prevPosX.size() > snakeLenght) prevPosX.pop_back(); //Sletter halens bagerste del
+    if (prevPosX.size() > snakeLenght) prevPosX.pop_back();
     if (prevPosY.size() > snakeLenght) prevPosY.pop_back();
 }
 
 int main() {
     int h = 1, v = 0;
+    system("cls");
     createGrid();
     while (true) {
-        cout << " Press WASD to move:" << endl;
-        for (int keyCode = 0; keyCode < 256; ++keyCode) { //Spillerens keystroke input
-            if (GetAsyncKeyState(keyCode) & 0x8000) {
-                char keyChar = static_cast<char>(keyCode);
-                if (keyChar == 'W' && v != 1) { h = 0; v = -1; }
-                if (keyChar == 'S' && v != -1) { h = 0; v = 1; }
-                if (keyChar == 'A' && h != 1) { v = 0; h = -1; }
-                if (keyChar == 'D' && h != -1) { v = 0; h = 1; }
-            }
-        }
+        if (GetAsyncKeyState('W') & 0x8000 && v != 1) { h = 0; v = -1; }
+        if (GetAsyncKeyState('S') & 0x8000 && v != -1) { h = 0; v = 1; }
+        if (GetAsyncKeyState('A') & 0x8000 && h != 1) { v = 0; h = -1; }
+        if (GetAsyncKeyState('D') & 0x8000 && h != -1) { v = 0; h = 1; }
+
         Sleep(100);
-        system("cls");
         moveSnake(h, v);
-        createGrid(); //Vis griddet til spilleren
+        createGrid();
     }
 }
